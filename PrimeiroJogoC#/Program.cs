03860d5f35389b7703f43f4797425aef9ce731fe
devfelipecesar <devfelipecesar@gmail.com>
2025-01-11 17:15:40 -0300

    Estrutura do jogo

--- PrimeiroJogoC#/Program.cs
+++ PrimeiroJogoC#/Program.cs
@@ -1,2 +1,23 @@
 ﻿int numeroSecreto = 42;
 
+do
+{
+    Console.Write("Digite um número entre 1 e 100: ");
+    int chute = int.Parse(Console.ReadLine());
+
+    if (chute == numeroSecreto)
+    {
+        Console.WriteLine("Parabéns!, Você acertou o número.");
+        break;
+    }
+    else if (chute < numeroSecreto)
+    {
+        Console.WriteLine("Número secreto é maior.");
+
+    }
+    else
+    {
+        Console.WriteLine("Número secreto é menor");
+    }
+}
+while (true);
